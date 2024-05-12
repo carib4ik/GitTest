6ca9357f972189e9e294eb951999df532c28fd1c
carib4ik <carib4ik@gmail.com>
2024-05-12 14:04:34 +0800

    feat: create simple calculator

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,35 @@
-﻿Console.WriteLine("test");
-Console.WriteLine("test");
-Console.WriteLine("test");
-Console.WriteLine("test");
+﻿while (true)
+{
+    Console.WriteLine("Первое число.");
+    var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Математическая операция:\n +\n -\n *\n /\n");
+    var operation = Console.ReadLine();
+
+    Console.WriteLine("Второе число.");
+    var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+    double result;
+    switch (operation)
+    {
+        case "+":
+            result = firstNumber + secondNumber;
+            Console.WriteLine("Результат: " + result);
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            Console.WriteLine("Результат: " + result);
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            Console.WriteLine("Результат: " + result);
+            break;
+        case "/":
+            result = firstNumber / secondNumber;
+            Console.WriteLine("Результат: " + result);
+            break;
+        default:
+            Console.WriteLine("Выбрана неверная операция.");
+            break;
+    }
+}
